4680adc5130532fffc88afcf150ac54a18974142
Juan Ramirez <juanpram2000@gmail.com>
2023-12-06 00:42:19 -0800

    Added a basic about section and some social media buttons. Social buttons might have a CSS change especially when I decide the theme of the website

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { Layouts } from "react-grid-layout";
 import ReactGridLayoutComponent from "./ReactGridLayoutComponent";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   const layouts: Layouts = {
@@ -32,32 +34,97 @@ export default function Home() {
         breakpoints={breakpoints}
         isResizable={false}
       >
-        <section key={"a"} className="bg-white rounded-xl p-3 h-full w-full">
-          <h1>Header</h1>
+        <section
+          key={"a"}
+          className="bg-stone-100 drop-shadow-xl shadow-xl rounded-xl p-3 h-full w-full flex flex-col gap-4"
+        >
+          <div className="flex flex-row w-full ">
+            <h1 className="text-5xl">Juan Ramirez</h1>
+          </div>
+          <div className="flex flex-row gap-2">
+            <button className="px-3 py-2 rounded-lg bg-sky-400 border-2 border-sky-800 transition-transform hover:scale-105 hover:bg-sky-600">
+              Full Stack Engineer
+            </button>
+            <button className="px-3 py-2 rounded-lg bg-emerald-400 border-2 border-emerald-800 transition-transform hover:scale-105 hover:bg-emerald-600">
+              Photographer
+            </button>
+          </div>
+          <p className="text-2xl w-full md:w-4/5 lg:w-2/3">
+            I like to play with websites, take pretty pics, and mess around with
+            things
+          </p>
         </section>
-        <section key={"b"} className="bg-white rounded-xl p-3 h-full w-full">
-          <ul>
-            <li>
-              <a href="https://github.com/JuanRamirez2000" target="_blank"></a>
+        <section
+          key={"b"}
+          className="bg-stone-100 drop-shadow-xl shadow-xl rounded-xl p-3 h-full w-full"
+        >
+          <ul className="h-full w-full grid grid-cols-3 grid-rows-1 md:grid-cols-1 md:grid-rows-3 lg:grid-cols-2 lg:grid-rows-2  gap-2">
+            <li className=" flex flex-col items-center justify-center">
+              <Link
+                className="p-4 rounded-2xl border-[3px] bg-sky-500 shadow-lg shadow-sky-600/50  border-sky-800 skew-y-12 transition-transform duration-100 hover:skew-y-0 hover:scale-105"
+                href="https://github.com/JuanRamirez2000"
+                target="_blank"
+              >
+                <Image
+                  src={"/assets/github_logo.svg"}
+                  alt="github logo"
+                  width={64}
+                  height={64}
+                />
+              </Link>
             </li>
-            <li>
-              <a href=""></a>
+            <li className="flex flex-col items-center justify-center">
+              <Link
+                href="https://www.instagram.com/juan_r_photos/"
+                className="p-4 rounded-2xl border-[3px] bg-sky-500 shadow-lg shadow-sky-600/50  border-sky-800 skew-y-12 transition-transform duration-100 hover:skew-y-0 hover:scale-105"
+                target="_blank"
+              >
+                <Image
+                  src={"/assets/instagram_logo.svg"}
+                  alt="instagram logo"
+                  width={64}
+                  height={64}
+                />
+              </Link>
             </li>
-            <li>
-              <a href=""></a>
+            <li className="flex flex-col items-center justify-center">
+              <Link
+                className="p-4 rounded-2xl border-[3px] bg-sky-500 shadow-lg shadow-sky-600/50  border-sky-800 skew-y-12 transition-transform duration-100 hover:skew-y-0 hover:scale-105"
+                href="https://www.linkedin.com/in/juanramirez2000/"
+                target="_blank"
+              >
+                <Image
+                  src={"/assets/linkedIn_logo.svg"}
+                  alt="linkedIn logo"
+                  width={64}
+                  height={64}
+                />
+              </Link>
             </li>
           </ul>
         </section>
-        <section key={"c"} className="bg-white rounded-xl p-3 h-full w-full">
+        <section
+          key={"c"}
+          className="bg-stone-100 drop-shadow-xl shadow-xl rounded-xl p-3 h-full w-full"
+        >
           <h1>Work</h1>
         </section>
-        <section key={"d"} className="bg-white rounded-xl p-3 h-full w-full">
+        <section
+          key={"d"}
+          className="bg-stone-100 drop-shadow-xl shadow-xl rounded-xl p-3 h-full w-full"
+        >
           <h1>Projects</h1>
         </section>
-        <section key={"e"} className="bg-white rounded-xl p-3 h-full w-full">
+        <section
+          key={"e"}
+          className="bg-stone-100 drop-shadow-xl shadow-xl rounded-xl p-3 h-full w-full"
+        >
           <h1>Tech</h1>
         </section>
-        <section key={"f"} className="bg-white rounded-xl p-3 h-full w-full">
+        <section
+          key={"f"}
+          className="bg-stone-100 drop-shadow-xl shadow-xl rounded-xl p-3 h-full w-full"
+        >
           <h1>Contact</h1>
         </section>
       </ReactGridLayoutComponent>
@@ -69,8 +136,8 @@ export default function Home() {
 //x (left -> right colms)
 //y (up -> down rows)
 const smLayout = [
-  { i: "a", x: 0, y: 0, w: 2, h: 6 },
-  { i: "b", x: 0, y: 1, w: 2, h: 2 },
+  { i: "a", x: 0, y: 0, w: 2, h: 6, static: true },
+  { i: "b", x: 0, y: 0, w: 2, h: 2 },
   { i: "c", x: 0, y: 2, w: 1, h: 8 },
   { i: "d", x: 1, y: 2, w: 1, h: 8 },
   { i: "e", x: 0, y: 3, w: 2, h: 6 },
@@ -78,8 +145,8 @@ const smLayout = [
 ];
 
 const mdLayout = [
-  { i: "a", x: 0, y: 0, w: 3, h: 6 },
-  { i: "b", x: 3, y: 0, w: 1, h: 6 },
+  { i: "a", x: 0, y: 0, w: 3, h: 6, static: true },
+  { i: "b", x: 3, y: 0, w: 1, h: 6, static: true },
   { i: "c", x: 0, y: 1, w: 2, h: 12 },
   { i: "d", x: 2, y: 1, w: 2, h: 6 },
   { i: "e", x: 2, y: 2, w: 2, h: 6 },
@@ -87,18 +154,18 @@ const mdLayout = [
 ];
 
 const lgLayout = [
-  { i: "a", x: 0, y: 0, w: 4, h: 6 },
-  { i: "b", x: 4, y: 0, w: 2, h: 6 },
+  { i: "a", x: 0, y: 0, w: 4, h: 6, static: true },
+  { i: "b", x: 4, y: 0, w: 2, h: 6, static: true },
   { i: "c", x: 0, y: 1, w: 3, h: 12 },
   { i: "d", x: 3, y: 1, w: 3, h: 10 },
   { i: "f", x: 3, y: 3, w: 3, h: 6 },
   { i: "e", x: 0, y: 3, w: 3, h: 4 },
 ];
 const xlLayout = [
-  { i: "a", x: 0, y: 0, w: 5, h: 6 },
-  { i: "b", x: 6, y: 0, w: 3, h: 6 },
+  { i: "a", x: 0, y: 0, w: 6, h: 6, static: true },
+  { i: "b", x: 6, y: 0, w: 2, h: 4, static: true },
   { i: "c", x: 0, y: 1, w: 3, h: 10 },
   { i: "d", x: 3, y: 1, w: 3, h: 6 },
-  { i: "e", x: 7, y: 1, w: 2, h: 10 },
+  { i: "e", x: 7, y: 1, w: 2, h: 12 },
   { i: "f", x: 3, y: 1, w: 3, h: 4 },
 ];
